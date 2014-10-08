db81ab671060d51d7b504ecf198a851c631447bb
Ivan Kelly <ivank@yahoo-inc.com>
2014-10-08 15:22:59 +0200

    Storing the log in bookkeeper

--- src/main/java/org/apache/bookkeeper/Dice.java
+++ src/main/java/org/apache/bookkeeper/Dice.java
@@ -1,27 +1,33 @@
 package org.apache.bookkeeper;
 
-import java.io.Closeable;
-import java.util.Random;
 
+import com.google.common.primitives.Ints;
+import java.io.Closeable;
+
+import java.util.Random;
+import org.apache.bookkeeper.conf.ClientConfiguration;
+import org.apache.bookkeeper.client.LedgerHandle;
+import org.apache.bookkeeper.client.BookKeeper;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.leader.LeaderSelector;
-import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 
 public class Dice extends LeaderSelectorListenerAdapter implements Closeable {
 
     final static String ZOOKEEPER_SERVER = "127.0.0.1:2181";
     final static String ELECTION_PATH = "/dice-elect";
+    final static byte[] DICE_PASSWD = "dice".getBytes();
 
     Random r = new Random();
     CuratorFramework curator;
     LeaderSelector leaderSelector;
+    BookKeeper bookkeeper;
 
     volatile boolean leader = false;
 
-
-    Dice() throws InterruptedException {
+    Dice() throws Exception {
         curator = CuratorFrameworkFactory.newClient(ZOOKEEPER_SERVER,
                 2000, 10000, new ExponentialBackoffRetry(1000, 3));
         curator.start();
@@ -30,12 +36,17 @@ public class Dice extends LeaderSelectorListenerAdapter implements Closeable {
         leaderSelector = new LeaderSelector(curator, ELECTION_PATH, this);
         leaderSelector.autoRequeue();
         leaderSelector.start();
+
+        ClientConfiguration conf = new ClientConfiguration()
+            .setZkServers(ZOOKEEPER_SERVER).setZkTimeout(30000);
+        bookkeeper = new BookKeeper(conf);
     }
 
     @Override
     public void takeLeadership(CuratorFramework client)
             throws Exception {
         synchronized (this) {
+            System.out.println("Becoming leader");
             leader = true;
             try {
                 while (true) {
@@ -54,17 +65,31 @@ public class Dice extends LeaderSelectorListenerAdapter implements Closeable {
         curator.close();
     }
 
-    void playDice() throws InterruptedException {
-        while (true) {
+    void lead() throws Exception {
+        LedgerHandle lh = bookkeeper.createLedger(3, 3, 2,
+                BookKeeper.DigestType.MAC, DICE_PASSWD);
+        try {
             while (leader) {
                 Thread.sleep(1000);
-                System.out.println("Value = " + (r.nextInt(6) + 1)
+                int nextInt = r.nextInt(6) + 1;
+                lh.addEntry(Ints.toByteArray(nextInt));
+                System.out.println("Value = " + nextInt
                                    + ", isLeader = " + leader);
+            }
+        } finally {
+            lh.close();
+        }
+    }
+
+    void playDice() throws Exception {
+        while (true) {
+            if (leader) {
+                lead();
             }
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         Dice d = new Dice();
         try {
             d.playDice();
